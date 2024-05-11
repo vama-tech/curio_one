e63c1a1839805e51a9f42ee5a7e6eac016b4d279
vama-tech <vamatechdept@gmail.com>
2024-05-11 12:06:06 +0530

    combined rviz with launch file

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -6,8 +6,9 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 
@@ -36,8 +37,21 @@ def generate_launch_description():
                                    '-entity', 'my_bot'],
                         output='screen')
     
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare("curio_one"),"config","robot.rviz"]
+    )
+
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output = "screen",
+        arguments=["-d",rviz_config_file,"use_sim_time:=true"]
+    )
+    
 
     return LaunchDescription([
             rsp,
             gazebo,
-            spawn_entity])
+            spawn_entity,
+            rviz])
