3544738710e453fa56f15e3a913ff655ba0e2429
viresompura <sompuraviren03@gmail.com>
2024-07-15 15:11:29 +0200

    Updated [IMU, TELEOP, SLAM]

--- launch/joystick.launch.py
+++ launch/joystick.launch.py
@@ -22,7 +22,7 @@ def generate_launch_description():
             executable='teleop_node',
             name='teleop_node',
             parameters=[joy_params, {'use_sim_time': use_sim_time}],
-            # remappings=[('/cmd_vel','/cmd_vel_joy')]
+            remappings=[('/cmd_vel','/cmd_vel_joy')]
          )
 
     # twist_stamper = Node(
